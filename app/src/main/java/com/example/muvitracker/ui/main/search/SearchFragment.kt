ff45669b99e44995f09966b35cb36d5dc359dc5e
dima <ologdm.job@gmail.com>
2024-11-24 16:37:50 +0100

    search fragment -> searchInput using flows - WIP

--- app/src/main/java/com/example/muvitracker/ui/main/search/SearchFragment.kt
+++ app/src/main/java/com/example/muvitracker/ui/main/search/SearchFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
-import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -12,8 +11,11 @@ import androidx.recyclerview.widget.GridLayoutManager
 import com.example.muvitracker.R
 import com.example.muvitracker.databinding.FragmSearchBinding
 import com.example.muvitracker.ui.main.Navigator
+import com.example.muvitracker.utils.fragmentViewLifecycleScope
 import com.example.muvitracker.utils.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -32,18 +34,20 @@ class SearchFragment : Fragment(R.layout.fragm_search) {
         onClickVHShow = { showIds ->
             navigator.startShowDetailFragment(showIds)
         },
-        onClickVHPerson = {personIds->
+        onClickVHPerson = { personIds ->
             navigator.startPersonFragmentFromSearch(personIds)
         })
 
+    // TODO: eliminare
     // Debouncing
-    private val handler = Handler(Looper.getMainLooper())
-    private var searchRunnable: Runnable? = null
-    private val DEBOUNCE_DELAY: Long = 300L
+//    private val handler = Handler(Looper.getMainLooper())
+//    private var searchRunnable: Runnable? = null
+//    private val DEBOUNCE_DELAY: Long = 300L
+
 
     // default values
-    private var filterValue: String = MOVIE_SHOW_PERSON
-    private var currentSearchText: String = ""
+//    private var filterValue: String = MOVIE_SHOW_PERSON // todo default su viewmodel
+//    private var currentSearchText: String = ""
 
 
     override fun onViewCreated(
@@ -51,53 +55,54 @@ class SearchFragment : Fragment(R.layout.fragm_search) {
         savedInstanceState: Bundle?
     ) {
 
-        viewModel.searchState.observe(viewLifecycleOwner) {
-            adapter.submitList(it)
-        }
+        // TODO: new OK
+        binding.searchEditText.doAfterTextChanged { s ->
+            // 3) cambia valore stringa + filter
+            viewModel.updateQuery(s.toString())
+            // chiamata risultati -> su viewmodel 4)
 
-        with(binding) {
-            recyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
-            recyclerView.adapter = adapter
-
-            searchEditText.doAfterTextChanged { s ->  // kotlin abbreviated method
-                currentSearchText = s.toString() // X
-
-                searchRunnable
-                    ?.let {// 1. cancel the previous runnable to implement debouncing
-                        handler.removeCallbacks(it)
-                    }
-                searchRunnable =
-                    Runnable { // 2. defines a new runnable that will perform the search
-                        viewModel.updateSearch(filterValue, currentSearchText) // X
-                    }
-                searchRunnable
-                    ?.let {// 3. schedule the new runnable with a specified delay to perform the debouncing
-                        handler.postDelayed(it, DEBOUNCE_DELAY)
-                    }
-            }
         }
 
 
-        // chip group
+        // chip group OK
         binding.searchChipGroup.isSingleSelection = true // selezione uno alla volta
         binding.searchChipGroup.isSelectionRequired = true // selezione uno alla volta
-        binding.searchChipGroup.check(R.id.chipAll) // default
+        binding.searchChipGroup.check(R.id.chipAll) // selezione chip default
         binding.searchChipGroup.setOnCheckedChangeListener { chipGroup, checkedId ->
-            filterValue = when (checkedId) {
+            val filterValue = when (checkedId) {
                 R.id.chipAll -> MOVIE_SHOW_PERSON
                 R.id.chipMovies -> MOVIE
                 R.id.chipShows -> SHOW
                 R.id.chipPeople -> PERSON
                 else -> MOVIE_SHOW_PERSON
             }
-            // Chiama ricerca con nuovo filtro
-            viewModel.updateSearch(filterValue, currentSearchText)
+
+            // 4 update filter value
+            viewModel.updateFilterValue(filterValue)
         }
+
+
+        // update list TODO old, elimina
+//        viewModel.searchResultState.observe(viewLifecycleOwner) {
+//            adapter.submitList(it)
+//        }
+        // TODO: da livedata a stateFlow
+        fragmentViewLifecycleScope.launch {
+            viewModel.searchResultState.collectLatest {listSearchResult ->
+                adapter.submitList(listSearchResult)
+                println(listSearchResult.toString())
+            }
+        }
+
+
+
+
+        // TODO paging3
     }
 
 
     companion object {
-        private const val MOVIE_SHOW_PERSON = "movie,show,person"
+        const val MOVIE_SHOW_PERSON = "movie,show,person"
         private const val MOVIE = "movie"
         private const val SHOW = "show"
         private const val PERSON = "person"
--- app/src/main/java/com/example/muvitracker/ui/main/search/SearchViewModel.kt
+++ app/src/main/java/com/example/muvitracker/ui/main/search/SearchViewModel.kt
@@ -5,7 +5,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.muvitracker.domain.model.SearchResult
 import com.example.muvitracker.domain.repo.SearchRepo
+import com.example.muvitracker.ui.main.search.SearchFragment.Companion.MOVIE_SHOW_PERSON
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,12 +21,49 @@ class SearchViewModel @Inject constructor(
     private val searchRepository: SearchRepo
 ) : ViewModel() {
 
-    val searchState = MutableLiveData<List<SearchResult>>()
+    companion object {
+        private const val DEBOUNCE_DELAY: Long = 300L
+    }
 
 
-    fun updateSearch(typeFilter: String, text: String) {
+    // 1) flow search
+    private val searchQueryFlow = MutableStateFlow("")
+
+    fun updateQuery(searchInput: String) {
+        searchQueryFlow.value = searchInput
+    }
+
+    // 2) flow filter
+    private val filterValueFlow = MutableStateFlow(MOVIE_SHOW_PERSON)
+
+    fun updateFilterValue(filterValue: String) {
+        filterValueFlow.value = filterValue
+    }
+
+
+    init {
+        // 4) fai chiamata -> quando dati cambiano, collect new result
         viewModelScope.launch {
-            searchState.value = searchRepository.getNetworkResult(typeFilter,text)
+            searchQueryFlow
+                .debounce(DEBOUNCE_DELAY)
+//                .filter { it.isNotBlank() }
+                .collectLatest {
+                    loadSearchResult(filterValueFlow.first(), it)
+                }
+        }
+
+    }
+
+
+    // RISULTATI
+    // TODO StarteContainer con result, no internet, ecc
+//    val searchResultState1 = MutableLiveData<List<SearchResult>>()
+    val searchResultState = MutableStateFlow<List<SearchResult>>(emptyList())
+
+    private fun loadSearchResult(typeFilter: String, text: String) {
+        viewModelScope.launch {
+            searchResultState.value = searchRepository.getNetworkResult(typeFilter, text)
+            println()
         }
     }
 
