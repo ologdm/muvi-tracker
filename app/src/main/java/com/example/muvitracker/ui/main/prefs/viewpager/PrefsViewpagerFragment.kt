785de5610c76b5f9ef807c90156dc7551a4d673a
dima <ologdm.job@gmail.com>
2025-11-24 18:49:30 +0100

    save last position on prefs

--- app/src/main/java/com/example/muvitracker/ui/main/prefs/viewpager/PrefsViewpagerFragment.kt
+++ app/src/main/java/com/example/muvitracker/ui/main/prefs/viewpager/PrefsViewpagerFragment.kt
@@ -1,28 +1,48 @@
 package com.example.muvitracker.ui.main.prefs.viewpager
 
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.muvitracker.R
 import com.example.muvitracker.databinding.FragmentPrefsViewpagerBinding
 import com.example.muvitracker.utils.viewBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import androidx.core.content.edit
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-class PrefsViewpagerFragment :Fragment(R.layout.fragment_prefs_viewpager){
+@AndroidEntryPoint
+class PrefsViewpagerFragment : Fragment(R.layout.fragment_prefs_viewpager) {
 
-    private val binding by viewBinding(FragmentPrefsViewpagerBinding::bind)
+    private val b by viewBinding(FragmentPrefsViewpagerBinding::bind)
+
+    @Inject
+    lateinit var sharedPrefs: SharedPreferences
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.viewPager.adapter = PrefsViewpagerAdapter(this)
+        b.viewPager.adapter = PrefsViewpagerAdapter(this)
+        b.viewPager.setCurrentItem(getLastTab(), false)
 
-        binding.tabLayout.tabMode = TabLayout.MODE_SCROLLABLE
-        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+        b.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                saveLastTab(position) // salva su shared
+            }
+        })
+
+
+        b.tabLayout.tabMode = TabLayout.MODE_SCROLLABLE
+
+        TabLayoutMediator(b.tabLayout, b.viewPager) { tab, position ->
             tab.text = when (position) {
                 0 -> getString(R.string.movies)
                 1 -> getString(R.string.shows)
@@ -31,19 +51,29 @@ class PrefsViewpagerFragment :Fragment(R.layout.fragment_prefs_viewpager){
             }
         }.attach()
 
+
         mainLayoutTopEdgeToEdgeManagment()
     }
 
+
+    private fun saveLastTab(position: Int) {
+        sharedPrefs.edit {
+            putInt("last_tab_prefs", position)
+        }
+    }
+
+
+    private fun getLastTab(): Int {
+        return sharedPrefs
+            .getInt("last_tab_prefs", 0) // default = 0
+    }
+
     private fun mainLayoutTopEdgeToEdgeManagment() {
-        ViewCompat.setOnApplyWindowInsetsListener(binding.mainLayout) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(b.mainLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             // aggiorno solo lati che mi servono
             v.updatePadding(top = systemBars.top)
             insets
         }
     }
-
-
-
-
 }
