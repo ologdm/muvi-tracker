4182ef2554a91a2353eba24d7bd4818074704d84
dima <ologdm.job@gmail.com>
2025-11-28 15:55:00 +0100

    clean up + note forzare lingua app

--- app/src/main/java/com/example/muvitracker/MyApp.kt
+++ app/src/main/java/com/example/muvitracker/MyApp.kt
@@ -2,37 +2,45 @@ package com.example.muvitracker
 
 import android.app.Application
 import android.content.Context
-import android.os.Build
-import com.bumptech.glide.Glide
-import com.example.muvitracker.data.LanguageManager
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 
 @HiltAndroidApp
 class MyApp : Application() {
 
-    // TODO 1.1.3 - funzioni per test lingue app
-    // Forza lingua app - bisogna sembre riaprire app
-//        val localeCode = "es"
-//        val localeCode = "it"
-//        val localeCode = "en"
-//        val localeCode = "fr"
-    val localeCode = "ro"
-//    val localeCode = "ru"
+    override fun onCreate() {
+        super.onCreate()
+        appContext = applicationContext
+    }
+
 
     companion object {
-        // per sharedPrefs
-        const val OLD_APP_LANGUAGE_PREF = "glide_language"
-
         // Context globale accessibile ovunque
         lateinit var appContext: Context
             private set
     }
 
+}
+
+
+
+/*  TODO: - solo per test forzare lingua !!!!!!!!! -------------------------------------------------------------
+@HiltAndroidApp
+class MyApp : Application() {
+
+    // Forza lingua app - bisogna sembre riaprire app
+//        val localeCode = "es"
+//        val localeCode = "it"
+//        val localeCode = "en"
+        val localeCode = "fr"
+//    val localeCode = "ro"
+//    val localeCode = "ru"
+
+
+    // !!! solo se forzo lingua !!!
+    // base:Context -> 'context grezzo' che Android fornisce all' app prima che venga inizializzata.
+    // cambiare la lingua prima -> le risorse vengono lette PRIMA dell’esecuzione di onCreate(), quindi serve fare qua il cambio
+    // IMP!!! se forzo sovrascrittura lingua, poi dopo 'Locale.getDefault().language' non restituisce lingua sistema
     override fun attachBaseContext(base: Context) {
         val newContext = LanguageManager.setAppLocale(base, localeCode)
         super.attachBaseContext(newContext)
@@ -40,42 +48,15 @@ class MyApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        appContext = applicationContext
 
+        // !!! solo se forzo lingua !!!
         // Per Android 13+ (API 33), applica anche qui la lingua
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            val localeManager = getSystemService(android.app.LocaleManager::class.java)
-            localeManager?.applicationLocales =
-                android.os.LocaleList.forLanguageTags(localeCode)
-        }
-
-        // 1.1.3 - invalida immagini glide
-        // Controllo e pulizia cache Glide se lingua cambiata
-        checkAndClearGlideCacheIfLanguageChanged()
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+//            val localeManager = getSystemService(android.app.LocaleManager::class.java)
+//            localeManager?.applicationLocales =
+//                android.os.LocaleList.forLanguageTags(localeCode)
+//        }
     }
-
-
-    // TODO - spostare in GlideUtils alla fine
-    private fun checkAndClearGlideCacheIfLanguageChanged() {
-        val prefs = getSharedPreferences("settings", Context.MODE_PRIVATE)
-        val savedLang = prefs.getString(OLD_APP_LANGUAGE_PREF, null)
-
-        if (savedLang != localeCode) {
-            // Pulizia cache disco
-            CoroutineScope(Dispatchers.IO).launch {
-                // 1. Pulizia cache disco su thread I/O - operazione può essere lunga, in background
-                Glide.get(this@MyApp).clearDiskCache()
-
-                // 2. Pulizia RAM cache sul main thread - perchè è collegata al thread UI di Android
-                withContext(Dispatchers.Main) {
-                    Glide.get(this@MyApp).clearMemory()
-                }
-
-                // Salva lingua corrente
-                prefs.edit().putString(OLD_APP_LANGUAGE_PREF, localeCode).apply()
-            }
-        }
-    }
-
 
 }
+ */
