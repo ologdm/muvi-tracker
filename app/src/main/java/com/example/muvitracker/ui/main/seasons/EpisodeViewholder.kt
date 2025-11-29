4107bcf292109735fbe4089ad5752c836629a084
dima <ologdm.job@gmail.com>
2025-11-29 22:48:42 +0100

    default values

--- app/src/main/java/com/example/muvitracker/ui/main/seasons/EpisodeViewholder.kt
+++ app/src/main/java/com/example/muvitracker/ui/main/seasons/EpisodeViewholder.kt
@@ -3,10 +3,12 @@ package com.example.muvitracker.ui.main.seasons
 import android.content.Context
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.muvitracker.R
+import com.example.muvitracker.data.utils.orIfNull
 import com.example.muvitracker.databinding.ViewholderEpisodeOnSeasonBinding
 import com.example.muvitracker.domain.model.Episode
 import com.example.muvitracker.utils.episodesFormatNumber
 import com.example.muvitracker.utils.formatDateFromFirsAired
+import com.example.muvitracker.utils.orIfNullOrBlank
 
 
 class EpisodeViewholder(
@@ -15,21 +17,21 @@ class EpisodeViewholder(
 
     private val context: Context = binding.root.context
 
-
-    fun bind(item: Episode) {
+    // TODO min null, default cases
+    fun bind(episode: Episode) {
         binding.episodeInfo.text =
             context.getString(
                 R.string.ep_number_first_aired_runtime_min,
-                item.episodeNumber.episodesFormatNumber(),
-                item.firstAiredFormatted.formatDateFromFirsAired(),
-                item.runtime
-            ) // TODO caso null con N/A
-//            "Episode ${item.episodeNumber.episodesFormatNumber()}"
+                episode.episodeNumber.episodesFormatNumber(),
+                episode.firstAiredFormatted.formatDateFromFirsAired().orIfNullOrBlank(
+                    context.getString(R.string.date_n_d)),
+                episode.runtime.orIfNull("—").toString()
+            )
 
         binding.episodeTitle.text =
-            "${item.title}"
+            episode.title.orIfNullOrBlank(context.getString(R.string.untitled))
 
-        binding.traktRating.text = item.traktRating
+        binding.traktRating.text = episode.traktRating.orIfNullOrBlank("-")
     }
 
 }
