ec2402d598ddc474c3b3ebc079cb7185e7c865a0
dima <ologdm.job@gmail.com>
2025-12-04 15:52:02 +0100

    create person entity and prefs (no add on db)

---
+++ app/src/main/java/com/example/muvitracker/data/database/entities/PrefsPersonEntity.kt
@@ -0,0 +1,13 @@
+//package com.example.muvitracker.data.database.entities
+//
+//import androidx.room.Entity
+//import androidx.room.PrimaryKey
+//
+//// TODO: 1.1.4 TODO
+//@Entity(tableName = "prefs_person_table")
+//data class PrefsPersonEntity(
+//    @PrimaryKey val traktId: Int,
+//    val liked: Boolean = false,
+//    val addedDateTime: Long, // using timestamp
+//    val notes : String = ""
+//)
