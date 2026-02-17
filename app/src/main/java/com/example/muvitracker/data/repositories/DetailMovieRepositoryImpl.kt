0c3e291022aa955b66b27977591b0eadae0560ed
dima <ologdm.job@gmail.com>
2026-02-17 22:52:34 +0100

    repo implementation

--- app/src/main/java/com/example/muvitracker/data/repositories/DetailMovieRepositoryImpl.kt
+++ app/src/main/java/com/example/muvitracker/data/repositories/DetailMovieRepositoryImpl.kt
@@ -1,6 +1,9 @@
 package com.example.muvitracker.data.repositories
 
 import com.dropbox.android.external.store4.StoreRequest
+import com.example.muvitracker.MyApp
+import com.example.muvitracker.R
+import com.example.muvitracker.data.MovieProvidersResponseDto
 import com.example.muvitracker.data.TmdbApi
 import com.example.muvitracker.data.TraktApi
 import com.example.muvitracker.data.database.MyDatabase
@@ -14,6 +17,7 @@ import com.example.muvitracker.data.utils.mapToIoResponse
 import com.example.muvitracker.data.utils.storeFactory
 import com.example.muvitracker.domain.model.CastAndCrew
 import com.example.muvitracker.domain.model.Movie
+import com.example.muvitracker.domain.model.Provider
 import com.example.muvitracker.domain.model.base.MovieBase
 import com.example.muvitracker.domain.repo.DetailMovieRepository
 import com.example.muvitracker.utils.IoResponse
@@ -22,8 +26,14 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import java.util.Locale
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.collections.component1
+import kotlin.collections.component2
+import kotlin.collections.joinToString
+import kotlin.collections.map
+import kotlin.collections.sortedBy
 import kotlin.coroutines.cancellation.CancellationException
 
 
@@ -135,4 +145,99 @@ class DetailMovieRepositoryImpl @Inject constructor(
         }
     }
 
+
+    override suspend fun getMovieProviders(movieId: Int): IoResponse<List<Provider>> {
+        // devo ritornare un IoResponse
+        return try {
+            val dto = tmdbApi.getMovieProviders(movieId)
+            IoResponse.Success(getProviderList(dto))
+        } catch (ex: CancellationException) {
+            // CancellationException - è l’eccezione usata da Kotlin per cancellare una coroutine.
+            throw ex // rilancio eccezione -> cancella la coroutines; se non la gestissi separatamente, rompo il meccanismo
+        } catch (ex: Throwable) {
+            IoResponse.Error(ex)
+        }
+    }
+
+
+    private fun getProviderList(response: MovieProvidersResponseDto): List<Provider> {
+        //        val region = "IT" // TODO test
+//        var responseDto = api.getMovieProviders(293660).results // TODO test
+        val region = Locale.getDefault().country
+        val regionProvidersDto = response.results[region] ?: return emptyList()
+
+        // 1. unisci le 4 liste come una mappa
+        // struttura come mappa key -> lista
+        val providersPair = listOf(
+            ProviderTypes.BUY to regionProvidersDto.buy,
+            ProviderTypes.STREAM to regionProvidersDto.flatrate,
+            ProviderTypes.RENT to regionProvidersDto.rent,
+            ProviderTypes.FREE to regionProvidersDto.free,
+            ProviderTypes.ADS to regionProvidersDto.ads,
+        ).filter { pair ->
+            pair.second.isNotEmpty() // emptyList default
+        }
+
+        // 2. da mappa a Pair<key, Obj>
+        // struttura key, elemento singolo
+        val flatProvidersPair = providersPair.flatMap { pair ->
+            val scomposto = pair.second.map { dto ->
+                pair.first to dto // == Pair(pair.first, dto)
+            }
+            scomposto
+        }
+
+        // 3. ragruppa Provider con id identico
+        val groupedProvidersMap =
+            flatProvidersPair.groupBy { pair ->
+                pair.second.providerId
+            }
+
+        // 4. (id -> Pair<Type,ProviderDto>)  => List<Provider>
+        // creo un provider per ogni entries + unisco i types
+        val domainProviders = groupedProvidersMap.map { (id, pairs) ->
+            // 1. ragruppo 'type', poi unisco in stringa
+            val types = pairs.joinToString(
+                separator = ", ",
+                transform = {
+                    it.first
+                })
+
+            val dto = pairs.first().second
+
+            // 2. creo un provider per ogni entry
+            val result = Provider(
+                providerId = id,
+                providerName = dto.providerName ?: "",
+                logoPath = dto.logoPath ?: "",
+                displayPriority = dto.displayPriority ?: 99, // vai in fondo
+                serviceType = types
+            )
+            result
+        }.sortedBy {
+            it.displayPriority
+        }
+
+        return domainProviders
+    }
 }
+
+
+object ProviderTypes {
+    var STREAM = MyApp.appContext.getString(R.string.textProviderStream)
+    var BUY = MyApp.appContext.getString(R.string.textProviderBuy)
+    var RENT = MyApp.appContext.getString(R.string.textProviderRent)
+    var ADS = MyApp.appContext.getString(R.string.textProviderAds)
+    var FREE = MyApp.appContext.getString(R.string.textProviderFree)
+
+}
+
+/*
+            "buy"  - Buy
+            "flatrate" - Streaming
+            "rent" - Rent
+            "free" - Free
+            "ads" - Advertising
+ */
+
+
--- app/src/main/java/com/example/muvitracker/domain/repo/DetailMovieRepository.kt
+++ app/src/main/java/com/example/muvitracker/domain/repo/DetailMovieRepository.kt
@@ -1,9 +1,11 @@
 package com.example.muvitracker.domain.repo
 
+import com.example.muvitracker.data.MovieProvidersResponseDto
 import com.example.muvitracker.data.database.entities.MovieEntity
 import com.example.muvitracker.data.dto._support.Ids
 import com.example.muvitracker.domain.model.CastAndCrew
 import com.example.muvitracker.domain.model.Movie
+import com.example.muvitracker.domain.model.Provider
 import com.example.muvitracker.domain.model.base.MovieBase
 
 import com.example.muvitracker.utils.IoResponse
@@ -18,6 +20,9 @@ interface DetailMovieRepository {
     suspend fun getRelatedMovies(movieId: Int): IoResponse<List<MovieBase>>
 
     suspend fun getMovieCast(movieId: Int): IoResponse<CastAndCrew> // contiene List<CastMember> -> personggi film
+
+
+    suspend fun getMovieProviders(movieId: Int): IoResponse<List<Provider>> // contiene List<CastMember> -> personggi film
 
 
 }
