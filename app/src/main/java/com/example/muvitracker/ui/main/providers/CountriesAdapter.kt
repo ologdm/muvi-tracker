945e572d220548699a9eba388a22ec359d6798d0
dima <ologdm.job@gmail.com>
2026-02-19 21:55:56 +0100

    todo - select country provider

---
+++ app/src/main/java/com/example/muvitracker/ui/main/providers/CountriesAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.muvitracker.ui.main.providers
+
+import android.widget.ListAdapter
+
---
+++ app/src/main/java/com/example/muvitracker/ui/main/providers/CountriesDialog.kt
@@ -0,0 +1,31 @@
+package com.example.muvitracker.ui.main.providers
+
+import android.os.Bundle
+import android.view.View
+import com.example.muvitracker.R
+import com.example.muvitracker.databinding.DialogCountriesBinding
+import com.example.muvitracker.databinding.FragmentEpisodeBottomsheetBinding.bind
+import com.example.muvitracker.utils.viewBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class CountriesDialog : BottomSheetDialogFragment(R.layout.dialog_countries) {
+
+    val b by viewBinding (DialogCountriesBinding::bind)
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // TODO: lista paesi da selezionare
+//        b.countriesRecycler.adapter =
+    }
+
+
+
+
+
+
+
+
+
+
+}
