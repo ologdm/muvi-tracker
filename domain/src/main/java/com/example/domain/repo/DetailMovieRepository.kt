9407a3386fbced0f2998057d217d644458e2f648
dima <ologdm.job@gmail.com>
2026-03-22 22:34:21 +0100

    clean up

--- domain/src/main/java/com/example/domain/repo/DetailMovieRepository.kt
+++ domain/src/main/java/com/example/domain/repo/DetailMovieRepository.kt
@@ -13,10 +13,6 @@ interface DetailMovieRepository {
 
     fun getSingleDetailMovieFlow(movieIds: Ids): Flow<IoResponse<Movie>>
 
-    // FIXME: fixare il toDomain, serve che la funzione sia nell'imterfaccia?  -> NOTE: -> no, e solo usata nel domain;
-//    fun getDetailListFlow(): Flow<List<MovieEntity?>>
-//    fun getDetailListFlow(): Flow<List<Movie>>
-
     suspend fun getRelatedMovies(movieId: Int): IoResponse<List<MovieBase>>
 
     suspend fun getMovieCast(movieId: Int): IoResponse<CastAndCrew> // contiene List<CastMember> -> personggi film
