cc433d58bd3ead7f73b2f127ea159b81c933a662
dima <ologdm.job@gmail.com>
2026-03-23 21:41:23 +0100

    move and add searchType on domain

---
+++ domain/src/main/java/com/example/domain/SearchType.kt
@@ -0,0 +1,10 @@
+package com.example.domain
+
+enum class SearchType(val value: String) {
+    MovieShowPerson("movie,show,person"),
+    Movie("movie"),
+    Show("show"),
+    Person("person")
+
+
+}
